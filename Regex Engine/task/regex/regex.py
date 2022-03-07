8e4f7a2801c5d8ee0d542b7b4273ec0bb0823ff4
Sergii Kamenskyi <serg@apple.com>
2022-03-07 22:12:32 +0200

    stage 3 completed!

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -6,20 +6,49 @@
 #     print("False")
 
 # ----------------------------------------STAGE 2------------------------------------------------------
+# def match(a, b):
+#     if a == '.' or a == b:
+#         return True
+#     return False
+#
+#
+# def compare(regex_, input_str_):
+#     if not regex_:
+#         return True
+#     if not input_str_:
+#         return False
+#     if not match(regex_[0], input_str_[0]):
+#         return False
+#     return compare(regex_[1:], input_str_[1:])
+#
+#
+# regex, input_str = input().split('|')
+# print(compare(regex, input_str))
+
+# ----------------------------------------STAGE 3------------------------------------------------------
 def match(a, b):
     if a == '.' or a == b:
         return True
     return False
 
 
-def compare(regex_, input_str_):
+def compare_same_length(regex_, input_str_):
     if not regex_:
         return True
     if not input_str_:
         return False
     if not match(regex_[0], input_str_[0]):
         return False
-    return compare(regex_[1:], input_str_[1:])
+    return compare_same_length(regex_[1:], input_str_[1:])
+
+
+def compare(regex_, input_str_):
+    while len(input_str_) >= len(regex_):
+        if compare_same_length(regex_, input_str_[0:len(regex_)]):
+            return True
+        else:
+            return compare(regex_, input_str_[1:])
+    return False
 
 
 regex, input_str = input().split('|')
