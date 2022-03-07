23be86610359a08d750c95ef4dead7eb17966479
Sergii Kamenskyi <serg@apple.com>
2022-03-07 18:51:44 +0200

    stage 1 completed!

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,1 +1,6 @@
-# write your code here
+# ----------------------------------------STAGE 1------------------------------------------------------
+regex, input_str = input().split('|')
+if regex == input_str or regex == '.' or not regex:
+    print("True")
+else:
+    print("False")
